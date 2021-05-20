a946d78a51353598ace1b4253ac6ac4def1ebae4
Barack <barackmukelenga100@gmail.com>
2021-05-20 21:51:23 +0200

    Finish the first part of the login and signup pages along with the header for the main home page

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,9 @@
+class UsersController < ApplicationController
+    def login 
+
+    end
+
+    def signup 
+
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root "home#index"
+  get '/signup', to: 'users#signup'
+  get '/login', to: 'users#login'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
